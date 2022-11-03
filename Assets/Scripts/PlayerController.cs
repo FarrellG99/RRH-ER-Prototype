3a92741ea3b8f596c61ec6a266d50d05cbd2db28
Tirza Artahsasta <tirza.asasta@gmail.com>
2022-11-03 23:06:06 +0700

    Movement2
    add ground check (overlap circle), coyote time (untested, not yet), physics material 2d (friction zero / friction = 0), change gravity for more punchy jump.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float moveSpeed;
     [SerializeField] float jumpForce;
     [SerializeField] float addSpeed;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckSize;
+    [SerializeField] LayerMask groundLayer;
     private Rigidbody2D playerRigidbody;
     private float runFasterTime = 0.5f;
     private float runFasterTimer;
+    private bool isGrounded;
+    private bool jumpInput;
+    private bool isJumping;
+
+    [Header("Coyote Time")]
+    [SerializeField] bool coyoteTime;
+    [SerializeField] float jumpBufferTime;
+    [SerializeField] float groundedTime;
+    private float lastGroundedTime;
+    private float lastJumpTime;
 
     [Header("Movement READONLY")]
     [SerializeField] private float moveForward;
@@ -28,17 +41,57 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        if(Input.GetButtonDown("Jump")) {
-            playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            Debug.Log("Jump!");
+        // Check if the circle overlaps with the ground.
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckSize, groundLayer);
+        jumpInput = Input.GetButtonDown("Jump");
+        if (coyoteTime)
+        {
+            if (isGrounded)
+            {
+                isJumping = false;
+                lastGroundedTime = groundedTime;
+            }
+            if (lastGroundedTime > 0 && lastJumpTime > 0 && !isJumping && jumpInput)
+            {
+                Jump();
+            }
+        } else {
+            if (jumpInput && isGrounded)
+            {
+                Jump();
+            }
         }
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        #region Coyote Timer
+        if(coyoteTime)
+        {
+            lastGroundedTime -= Time.deltaTime;
+            lastJumpTime -= Time.deltaTime;
+        }
+        #endregion
+
+        Move();
+    }
+
+    void Jump()
+    {
+        if(coyoteTime)
+        {
+            isJumping = true;
+        }
+        playerRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        Debug.Log("Jump!");
+    }
+
+    void Move()
+    {
         runFasterTimer -= Time.deltaTime;
-        if(runFasterTimer <= 0)
+        if (runFasterTimer <= 0)
         {
             runFasterTimer = runFasterTime;
             moveForward += addSpeed;
