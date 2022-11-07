0324fcded151522a31845ac56f7f819dde029029
Tirza Artahsasta <tirza.asasta@gmail.com>
2022-11-07 14:50:18 +0700

    Movement4
    Slide (fix, tested)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("temp")]
+    [SerializeField] Transform model;
+
     [Header("Movement")]
     [SerializeField] float moveSpeed;
-    [SerializeField] float jumpForce;
     [SerializeField] float addSpeed;
+    private Rigidbody2D playerRigidbody;
+    private CapsuleCollider2D playerCollider;
+    private float runFasterTime = 0.5f;
+    private float runFasterTimer;
+
+    [Header("Jump")]
+    [SerializeField] float jumpForce;
     [SerializeField] Transform groundCheck;
     [SerializeField] float groundCheckSize;
     [SerializeField] LayerMask groundLayer;
-    private Rigidbody2D playerRigidbody;
-    private float runFasterTime = 0.5f;
-    private float runFasterTimer;
     private bool isGrounded;
     private bool jumpInput;
     private bool jumpInputReleased;
     private bool isJumping;
+
+    [Header("Slide")]
+    [SerializeField] float slideTime;
+    [SerializeField] Quaternion slidingTargetRotation;
+    [SerializeField] Vector2 colliderTargetSize;
+    private bool slideInput;
+    private bool slideInputRelease;
+    private bool isSliding;
+    private float slidingTimer;
+    private Quaternion originalRotation;
+    private Vector2 colliderOriginalSize;
 
     [Header("JumpCut")]
     [SerializeField] bool jumpCut;
@@ -38,9 +55,20 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Rigidbody
         playerRigidbody = GetComponent<Rigidbody2D>();
+
+        // Collider and get collider original size
+        playerCollider = GetComponent<CapsuleCollider2D>();
+        colliderOriginalSize = playerCollider.size;
+
+        // Get original rotation
+        originalRotation = transform.rotation;
+
+        // Setting move forward, run faster timer, sliding timer
         moveForward = moveSpeed;
         runFasterTimer = runFasterTime;
+        slidingTimer = slideTime;
 
         //Debug.Log(moveForward);
     }
@@ -51,12 +79,15 @@ public class PlayerController : MonoBehaviour
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckSize, groundLayer);
         jumpInput = Input.GetButtonDown("Jump");
         jumpInputReleased = Input.GetButtonUp("Jump");
+        slideInput = Input.GetButtonDown("Slide");
+        slideInputRelease = Input.GetButtonUp("Slide");
         
         if(isGrounded)
         {
             isJumping = false;
         }
 
+        // Jump with or without coyote time
         if (coyoteTime)
         {
             if (isGrounded)
@@ -77,11 +108,26 @@ public class PlayerController : MonoBehaviour
             }
         }
 
+        // Jump Cut
         if (jumpInputReleased && jumpCut)
         {
             JumpCut();
         }
 
+        // Sliding
+        if(slideInput)
+        {
+            isSliding = true;
+            SlideCheck();
+        }
+
+        if(slideInputRelease || slidingTimer <= 0)
+        {
+            isSliding = false;
+            SlideCheck();
+        }
+
+        
     }
 
     // Update is called once per frame
@@ -94,6 +140,12 @@ public class PlayerController : MonoBehaviour
             lastJumpTime -= Time.deltaTime;
         }
         #endregion
+
+        if(isSliding)
+        {
+            slidingTimer -= Time.deltaTime;
+        }
+
         Move();
     }
 
@@ -135,5 +187,31 @@ public class PlayerController : MonoBehaviour
         }
 
         playerRigidbody.velocity = new Vector2(moveForward * Time.deltaTime, playerRigidbody.velocity.y);
+    }
+
+    void SlideCheck()
+    {
+        slidingTimer = slideTime;
+
+        if (isSliding)
+        {
+            // Rotation just for animation
+            model.rotation = Quaternion.Lerp(transform.rotation, slidingTargetRotation, 1f);
+
+            // Change Collider Size
+            playerCollider.direction = CapsuleDirection2D.Horizontal;
+            playerCollider.size = colliderTargetSize;
+
+            Debug.Log("Sliding");
+        } else if (!isSliding)
+        {
+            // Rotation just for animation
+            model.rotation = Quaternion.Lerp(transform.rotation, originalRotation, 1f);
+
+            // Change Collider Size
+            playerCollider.direction = CapsuleDirection2D.Vertical;
+            playerCollider.size = colliderOriginalSize;
+            Debug.Log("Stop sliding");
+        }
     }
 }
